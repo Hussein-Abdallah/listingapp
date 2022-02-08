28b13e799826ce074d6fd71dc0fb543b4a7b3652
Sain <sain.abdallah@gmail.com>
2022-02-08 17:33:44 -0500

    Initiate App and create folders structure

---
+++ src/screens/Welcome/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Welcome(props) {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default Welcome;
